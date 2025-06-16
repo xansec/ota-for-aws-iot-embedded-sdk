b440806d811930a33226f39f92078bca9f81e088
xansec <xansec@users.noreply.github.com>
2025-06-16 18:03:47 +0000

    Doxygen for 477ee9a556bb2e44cc06b424c78670e17b759e81

---
+++ main/search/functions_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['filesizetoblocks_0',['fileSizeToBlocks',['../ota_8c.html#af07f5a44a626cfb04eaf9d8938c3b18b',1,'ota.c']]],
+  ['freefilecontextmem_1',['freeFileContextMem',['../ota_8c.html#ae466af59782893adbc54667423fe266f',1,'ota.c']]]
+];
---
+++ main/search/functions_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['getfilecontextfromjob_0',['getFileContextFromJob',['../ota_8c.html#a42dd2fd46df4b4b09954c55e8e237cc4',1,'ota.c']]]
+];
